d82c26b428062b54b1068c7a577d9332d802e0b4
Alberto <alberto.c.pueblas@gmail.com>
2024-05-14 18:17:11 +0200

    create endpoint user profile, user appointment and update user profile, and routes

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,55 +1,18 @@
 import { Request, Response } from "express"
 import { Users } from "../models/Users";
 import { UserRole } from "../constants/UserRole";
+import bcrypt from "bcrypt";
 
-
+//------------------------------------------------
 
 
 export const userController = {
 
-    async getAllUsers ( req: Request, res: Response ): Promise<void> {
-        try {
-            //Pagination
-            const page = Number(req.query.page) || 1;
-            const limit = Number(req.query.limit) || 25;
-
-            const [users, totalUsers] = await Users.findAndCount({
-                select: {
-                    id: true,
-                    firstName: true,
-                    lastName: true,
-                    email: true,
-                    phone: true,
-                },
-                skip: (page - 1) * limit,
-                take: limit,
-            });
-            if (users.length === 0) {
-                res.status(404).json({
-                    message: "Users not found"
-                });
-                return;
-            }
-            const totalPages = Math.ceil(totalUsers/ limit);
-            res.status(200).json({
-                users: users,
-                current_page: page,
-                per_page: limit,
-                total_pages: totalPages,
-            });
-        } catch (error) {
-            res.status(500).json({
-                message: "ups!, something went wrong"
-            });
-            
-        }
-    },
-
-    async getAllStylist (req: Request, res: Response): Promise<void> {
+    async getAllStylist(req: Request, res: Response): Promise<void> {
         try {
             const stylistId = UserRole.STYLIST
 
-            const [ stylist, totalStylist ] = await Users.findAndCount({
+            const [stylist] = await Users.findAndCount({
                 select: {
                     id: true,
                     firstName: true,
@@ -75,7 +38,206 @@ export const userController = {
             res.status(500).json({
                 message: "ups!, something went wrong"
             });
-            
+
+        }
+    },
+
+    async getAllUsers(req: Request, res: Response): Promise<void> {
+        try {
+            //Pagination
+            const page = Number(req.query.page) || 1;
+            const limit = Number(req.query.limit) || 25;
+
+            const [users, totalUsers] = await Users.findAndCount({
+                select: {
+                    id: true,
+                    firstName: true,
+                    lastName: true,
+                    email: true,
+                    phone: true,
+                },
+                skip: (page - 1) * limit,
+                take: limit,
+            });
+            if (users.length === 0) {
+                res.status(404).json({
+                    message: "Users not found"
+                });
+                return;
+            }
+            const totalPages = Math.ceil(totalUsers / limit);
+            res.status(200).json({
+                users: users,
+                current_page: page,
+                per_page: limit,
+                total_pages: totalPages,
+            });
+        } catch (error) {
+            res.status(500).json({
+                message: "ups!, something went wrong"
+            });
+
+        }
+    },
+
+    async getAppointmentById(req: Request, res: Response): Promise<void> {
+        try {
+            const userId = Number(req.params.id);
+
+            const user = await Users.findOne({
+                select: {
+                    id: true,
+                    firstName: true,
+                    lastName: true,
+                    email: true,
+                    phone: true,
+                },
+                relations: {
+                    clientDates: {
+                    }
+                },
+                where: {
+                    id: userId
+                }
+            });
+
+            if (!user) {
+                res.status(400).json({
+                    message: "User not found"
+                })
+                return;
+            }
+
+            res.json(user);
+        } catch (error) {
+
+        }
+    },
+
+    async getUserById(req: Request, res: Response): Promise<void> {
+        try {
+            const userId = Number(req.params.id);
+
+            const user = await Users.findOne({
+                select: {
+                    id: true,
+                    firstName: true,
+                    lastName: true,
+                    email: true,
+                    phone: true,
+                },
+                where: {
+                    id: userId
+                }
+            });
+
+            if (!user) {
+                res.status(400).json({
+                    message: "User not found"
+                })
+                return;
+            }
+
+            res.json(user);
+        } catch (error) {
+            res.status(500).json({
+                message: "Failed to retrive user"
+            });
+        }
+    },
+
+    async getMeAppointment(req: Request, res: Response): Promise<void> {
+        try {
+            const userId = Number(req.tokenData.userId);
+
+            const user = await Users.findOne({
+                select: {
+                    id: true,
+                    firstName: true,
+                    lastName: true,
+                    email: true,
+                    phone: true,
+                    clientDates: true,
+                },
+                relations: {
+                    clientDates: {
+                    }
+                },
+                where: {
+                    id: userId
+                }
+            });
+            res.json(user);
+
+        } catch (error) {
+            res.json(500).json({
+                message: "Failed to retrive appointment"
+            })
+        }
+    },
+
+    async getMeProfile(req: Request, res: Response): Promise<void> {
+        try {
+            const userId = Number(req.tokenData.userId);
+
+            const user = await Users.findOne({
+                select: {
+                    id: true,
+                    firstName: true,
+                    lastName: true,
+                    email: true,
+                    phone: true,
+                },
+                where: {
+                    id: userId
+                }
+            });
+
+            res.json(user);
+
+        } catch (error) {
+            res.status(500).json({
+                message: "ups! profile not found"
+            })
+            return;
+        }
+    },
+
+    async updateProfile(req: Request, res: Response): Promise<void> {
+        try {
+            const userId = Number(req.tokenData.userId);
+
+            const { password, ...resUserData } = req.body;
+
+            const userToUpdate = await Users.findOne({ where: { id: userId } });
+
+            if (!userToUpdate) {
+                res.status(404).json({
+                    message: "User not found"
+                });
+                return;
+            }
+
+            if (password) {
+                const hashedPassword = bcrypt.hashSync(password, 10);
+                userToUpdate!.password = hashedPassword;
+            }
+
+            const updatedUser: Partial<Users> = {
+                ...userToUpdate,
+                ...resUserData,
+            };
+
+            await Users.save(updatedUser)
+
+            res.status(202).json({
+                message: "User has been updated",
+            });
+
+        } catch (error) {
+            res.status(500).json({
+                message: "Failed to update dates",
+            });
         }
     }
 };
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -11,6 +11,18 @@ const router = express.Router();
 
 router.get("/allUsers", auth, authorize(["Admin", "Stylist"]), userController.getAllUsers);
 router.get("/allStylist", auth, authorize(["Admin"]), userController.getAllStylist);
+router.get("/user/:id", auth, authorize(["Admin"]), userController.getUserById);
+router.get("/appointment/:id", auth, authorize(["Admin", "Stylist"]), userController.getAppointmentById);
+
+
+
+
+
+
+//Client routes.
+router.get("/profile", auth, userController.getMeProfile);
+router.get("/appointment", auth, userController.getMeAppointment);
+router.put("/putProfile", auth, userController.updateProfile);
 
 
 export default router
